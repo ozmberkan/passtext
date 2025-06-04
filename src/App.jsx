63529970555e374cb2a939cd1634231b2b0b8a48
Berkan <148571945+ozmberkan@users.noreply.github.com>
2025-06-04 11:39:28 +0300

    feat: add zustand for state management and implement user authentication flow

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import NotFound from "./pages/404/NotFound";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
 import Home from "./pages/Home/Home";
+import { useAuthStore } from "./store/useAuthStore";
 
 const App = () => {
+  const user = useAuthStore((state) => state.user);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="*" element={<NotFound />} />
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={user ? <Home /> : <Navigate to="/kayit-ol" />}
+        />
         {/* Auth */}
         <Route path="/giris-yap" element={<Login />} />
         <Route path="/kayit-ol" element={<Register />} />
