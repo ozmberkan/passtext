468d77b9e5162e378b0f5eabb0f0c2d5e499705c
Berkan <148571945+ozmberkan@users.noreply.github.com>
2025-06-04 17:43:06 +0300

    feat: update Menu component styling; adjust header layout and improve accessibility

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -15,9 +15,9 @@ const Menu = () => {
   const loaded = useAuthStore((state) => state.loaded);
   return (
     <div className="bg-neutral-100 min-w-[200px] flex flex-col items-start justify-start  rounded-md p-3 h-full border">
-      <h1 className="text-2xl font-bold flex gap-1 justify-center items-center  tracking-tighter">
-        <TbFingerprint className="animate-pulse text-zinc-500" />
-        Passtext
+      <h1 className="text-2xl px-2 py-1.5 font-bold flex gap-1 justify-center items-center  tracking-tighter">
+        <TbFingerprint size={18} className="animate-pulse text-neutral-700" />
+        <span className="text-sm">Passtext</span>
       </h1>
       <Separator className="my-2" />
       <nav className="mt-2 w-full">
